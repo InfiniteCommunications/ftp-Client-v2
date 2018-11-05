1b85b4295adbef244a1d593070a3a4f402bccb58
Wan Chin <wan.cin093@gmail.com>
2018-11-05 11:10:24 +0800

    MASTER UPLOAD

---
+++ FTPClient/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FTPClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += onLoad;
+        }
+
+        private void onLoad(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Maximized;
+            ResizeMode = ResizeMode.NoResize;
+            Loaded -= onLoad;
+        }
+
+        //public void SoundRecording()
+        //{
+        //    [DllImport("winmm.dll", EntryPoint = "mciSendStringA", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        //    private static extern int mciSendString(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+        //    System.Timers.Timer timer1 = new System.Timers.Timer();
+        //    List<System.Windows.Media.MediaPlayer> sounds = new List<System.Windows.Media.MediaPlayer>();
+        //}
+    }
+}
